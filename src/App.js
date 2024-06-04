cb816a223403c357d9cbe0b7ce73281d9a513867
rksrbbn <rakasantang22@gmail.com>
2024-06-04 20:47:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {members} from './membersData';
+import {unitSongs} from './unitSongs';
+import { Container, FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material';
 
 function App() {
+  const [selectedSong, setSelectedSong] = useState('');
+  const [selectedMembers, setSelectedMembers] = useState([]);
+  const [unitSongMembers, setUnitSongMembers] = useState(0);
+
+  const handleSongChange = (event) => {
+    const selectedSongName = event.target.value;
+    setSelectedSong(selectedSongName);
+    
+    // Temukan unit song yang dipilih
+    const selectedUnitSong = unitSongs.find(song => song.name === selectedSongName);
+
+    if (selectedSongName !== '') {
+      setUnitSongMembers(selectedUnitSong.total_member);
+    }
+    else
+    {
+      setUnitSongMembers(0);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container className="App">
+      <header>
+        <h1>JKT48 Lineup</h1>
       </header>
-    </div>
+
+      {/* buat selection unit song menggunakan material-ui */}
+      <FormControl fullWidth style={{ marginBottom: '20px' }}>
+        <InputLabel id="song-select-label">Pilih lagu</InputLabel>
+        <Select
+          labelId="song-select-label"
+          value={selectedSong}
+          onChange={handleSongChange}
+          displayEmpty
+        >
+          <MenuItem value="">
+            <em>Pilih lagu</em>
+          </MenuItem>
+          {unitSongs.map((song) => (
+            <MenuItem key={song.name} value={song.name}>{song.name}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      
+      {selectedSong && (
+        <FormControl fullWidth style={{ marginBottom: '20px' }}>
+          <InputLabel id="member-multi-select-label">Pilih member</InputLabel>
+          <Select
+            labelId="member-multi-select-label"
+            multiple
+            value={selectedMembers}
+            onChange={(event) => {
+              const {
+                target: { value },
+              } = event;
+              // Batasi jumlah member yang dipilih sesuai dengan unitSongMembers
+              const selected = typeof value === 'string' ? value.split(',') : value;
+              if (selected.length <= unitSongMembers) {
+                setSelectedMembers(selected);
+              } else {
+                // Jika melebihi, potong array untuk membatasi jumlahnya
+                setSelectedMembers(selected.slice(0, unitSongMembers));
+              }
+            }}
+            renderValue={(selected) => selected.join(', ')}
+            MenuProps={{
+              PaperProps: {
+                style: {
+                  maxHeight: 48 * 4.5 + 8,
+                  width: 250,
+                },
+              },
+            }}
+          >
+            {members.map((member) => (
+              <MenuItem key={member.name} value={member.alias}>
+                {member.alias}
+              </MenuItem>
+            ))}
+          </Select>
+          <Button variant="outlined" color="error" onClick={() => setSelectedMembers([])} style={{ marginTop: '20px' }}>Clear Members</Button>
+        </FormControl>
+      )}
+     
+    </Container>
   );
 }
 
---
+++ src/unitSongs.js
@@ -0,0 +1,27 @@
+export const unitSongs = [
+    {
+        name: 'Ekor Malaikat',
+        setlist: 'Pajama Drive',
+        theme: 'cute',
+        dance_level: 1,
+        vocal_level: 1,
+        total_member: 3,
+    },
+    {
+        name: 'Prinsip Kesucian Hati',
+        setlist: 'Pajama Drive',
+        theme: 'sexy',
+        dance_level: 1,
+        vocal_level: 1,
+        total_member: 3,
+    },
+    {
+        name: 'Joan DArc Didalam Cermin',
+        setlist: 'Pajama Drive',
+        theme: 'cool',
+        dance_level: 1,
+        vocal_level: 1,
+        total_member: 5,
+    },
+];
+
